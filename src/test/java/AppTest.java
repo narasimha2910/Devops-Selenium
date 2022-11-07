e58dc4faa08d9791a6c105ca8e3e36399c661873
narasimha2910 <2910.narasimha@gmail.com>
2022-11-07 19:36:41 +0530

    GitHub Actions

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,6 +1,7 @@
 package test;
 
 import java.util.concurrent.TimeUnit;
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -10,21 +11,20 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class AppTest 
 { 
 	public WebDriver driver; 
-	public String baseUrl = "https://www.lambdatest.in/";  
+	public String baseUrl = "https://www.google.com/";  
 	
 	@Test             
 	public void test1() {      
 		
 		WebDriverManager.chromedriver().setup();
 		ChromeOptions options = new ChromeOptions();
-		options.addArguments("--no-sandbox");
-		options.addArguments("--disable-dev-shm-usage");
-		options.addArguments("--headless");
 		driver = new ChromeDriver(options);
- 
-		driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);  
-		driver.manage().window().maximize();  
+		driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
 		driver.get(baseUrl);
+		String actualTitle = driver.getTitle();
+		driver.manage().window().maximize();
+		String expectedTitle = "Google";
+		Assert.assertEquals("Condition true", actualTitle, expectedTitle);
 		driver.close();
 	}     
 }
